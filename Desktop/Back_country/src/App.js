95b8f8bb19fa425c2223919e7500b13be3063112
Jayden <jaydenowens23@gmail.comgit config --global core.editor vimtouch ~/.gitignorecode ~/.gitignoregit config --global core.excludesfile ~/.gitignore>
2021-03-31 12:49:11 -0600

    new

---
+++ Desktop/Back_country/src/App.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+import Home from './components/Home';
+
+class App extends Component{
+  render() {
+    return(
+      <>
+        <h1 > </h1>
+      </>
+    )
+  }
+}
+
+
+export default App;
